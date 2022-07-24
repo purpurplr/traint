f314c2d5b83a6c53575ffaae26747785d4ab89d6
Aliaksei Auramionak <aliaksei_auramionak2@epam.com>
2022-07-24 14:07:29 +0300

    chore: use babel to transpile jsx

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ import { Environment } from '@environment/environment.interface';
 
 export const environment: Environment = {
   api: {
-    roadmap: 'https://traint-7a403-default-rtdb.europe-west1.firebasedatabase.app/.json',
+    roadmap: 'https://traint-demo-default-rtdb.europe-west1.firebasedatabase.app/.json',
   },
 };
