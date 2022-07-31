a6c335c3add2fc179727a7e16a6b1abd7dd4d4cf
Aliaksei Auramionak <aliaksei_auramionak2@epam.com>
2022-07-31 15:40:35 +0300

    feat: router

--- src/app/layout/header/header.component.tsx
+++ src/app/layout/header/header.component.tsx
@@ -3,12 +3,14 @@ import { JSX } from 'preact';
 import LogoIcon from '@assets/icons/logo.svg';
 
 import './header.component.scss';
+import { ThemeToggler } from '@global-features/theme/components/theme-toggler/theme-toggler.component';
 
 export const HeaderComponent = (): JSX.Element => (
   <header className="container">
     <h1 className="app-header">
       <LogoIcon className="app-header__logo" />
       <span className="app-header__text">Карта знаний</span>
+      <ThemeToggler />
     </h1>
   </header>
 );
