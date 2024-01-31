c89626a1c03ca10bab2696dc2bcbfae21f765143
Aliaksandr Mirankou <alexandermirankov@gmail.com>
2024-01-31 21:58:34 +0400

    (no test): redeploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
+  base: '/dist/',
   plugins: [svgr({ exportAsDefault: true }), react(), tsconfigPaths(), visualizer()],
 });
